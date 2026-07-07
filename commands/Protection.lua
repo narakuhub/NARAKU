b56d68bf91af36cf72a5e05c590c70bafa4564ed
narakuhub <narakuhub@gmail.com>
2026-07-08 05:07:30 +0700

    Update Protection.lua

--- commands/Protection.lua
+++ commands/Protection.lua
@@ -1,1 +1,20 @@
+-- Protection.lua
 
+local Protection = {}
+
+function Protection.StartAntiAFK()
+    local VirtualUser = game:GetService("VirtualUser")
+    local Players = game:GetService("Players")
+
+    local Player = Players.LocalPlayer
+
+    Player.Idled:Connect(function()
+        VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    end)
+
+    return true
+end
+
+return Protection
